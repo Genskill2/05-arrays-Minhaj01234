1d82553a12da4a3f921ec56b293174add4bfcace
Minhaj01234 <84147350+Minhaj01234@users.noreply.github.com>
2021-06-16 21:01:08 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -2,21 +2,4 @@
 #include <stdio.h>
 
 
- int max(int largest_int[],int x);
-
-   int main(void) {
-   int x,i,; 
-   int largest_int[]= {1  3 5  12 6 9 7};
-int max(int largest_int[],int x);
-  for(i=x;i>x;i++)
-  {
-    if(i>x)
-    {
-   
-     printf("%d\n",i);
-
-            }
-            }
-       return i;     
-            
-            }
+ 
