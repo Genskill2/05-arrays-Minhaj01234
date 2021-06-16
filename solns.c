9e5e26eeae93d0b0c03a5e2c32d5fc9cfc4dc479
Minhaj01234 <84147350+Minhaj01234@users.noreply.github.com>
2021-06-16 20:56:39 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,22 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+
+ int max(int largest_int[],int x);
+
+   int main(void) {
+   int x,i,y; 
+   int largest_int[]= {1  3 5  12 6 9 7};
+int max(int largest_int[],int x);
+  for(i=0;i>y;i++)
+  {
+    if(i>y)
+    {
+   
+     printf("%d\n",i);
+
+            }
+            }
+       return i;     
+            
+            }
