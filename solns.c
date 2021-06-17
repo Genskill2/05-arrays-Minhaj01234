114bb17f9a4b5d5b1c93d60e771e67b30e1a129e
Minhaj01234 <84147350+Minhaj01234@users.noreply.github.com>
2021-06-17 22:09:01 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,5 +1,16 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 
+int_max(int [x],int y);
 
- 
+int main (void) {
+	int max,x,y;
+	int [x] = {9,5,6,10,2,-3,4};
+	int_max(int [x],int y);
+	
+	for (int i=x;i>x;i++)
+	{
+		prinf("%d is largest",y);
+	}
+}
+
